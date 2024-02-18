4b34b71b6dd09a020e761dc3243a8b3c5c1b60a1
JIAN LEE <jwm0307@daum.net>
2024-02-18 21:10:26 +0900

    Update 숨바꼭질.py

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,18 +1,20 @@
 from collections import deque
-# BFS
 def bfs():
     q = deque()
     q.append(n)
-    while q :
+    while q:
         now = q.popleft()
-        if now == k :
-            print(road[k])
+        if now == k:
+            print(road[k]-1)  # 시작점 표시했던 것 하나 빼기
             break
-        for next in (now-1, now+1, now*2):
-            if 0<= next <= 10**5 and not road[next]:
+        for next in (now * 2, now - 1, now + 1):
+            if 0 <= next <= 10 ** 5 and not road[next]:
                 road[next] = road[now] + 1
                 q.append(next)
 
+
 n, k = map(int, input().split())
-road = [0] * (10**5+1)
+road = [0] * (10 ** 5 + 1)
+## 시작점 표시!!!(0에서 시작한다면 *2하면 0이 되기 때문에 1로 미리 바꿔주기)
+road[n] = 1
 bfs()
