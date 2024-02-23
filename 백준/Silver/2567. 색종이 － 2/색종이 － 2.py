a3d742a2c0bcb8b2940c6a47a215ac5a79ca8478
JIAN LEE <jwm0307@daum.net>
2024-02-23 21:55:20 +0900

    [Silver IV] Title: 색종이 - 2, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,10 +1,4 @@
-# 겉 직사각형 둘레 + 안직사각형둘레
-
 n = int(input())
-
-# 0으로된 이차원배열
-# 색종이부분 1로 다 바꾸기
-# 겉에있는 1 갯수구하기 (사방에 0 있는 갯수구하기)
 
 arr = [[0]*102 for _ in range(102)]
 for _ in range(n):
@@ -13,10 +7,10 @@ for _ in range(n):
         for j in range(y, y+10):
             arr[i][j] = 1
 cnt = 0
-for i in range(100):
-    for j in range(100):
-        if arr[i][j] == 1:
-            for dx, dy in ((-1,0),(1,0),(0,-1),(0,1)):
-                if arr[i+dx][j+dy] == 0:
-                    cnt += 1
+for i in range(101):
+    for j in range(101):
+        if arr[i][j] != arr[i+1][j] :
+            cnt += 1
+        if arr[i][j] != arr[i][j+1]:
+            cnt += 1
 print(cnt)
