c8150869d7188e5047109f1d225b094df68101ff
JIAN LEE <jwm0307@daum.net>
2024-02-27 22:42:56 +0900

    [D3] Title: 영준이의 카드 카운팅, Time: 146 ms, Memory: 58,516 KB -BaekjoonHub

--- SWEA/D3/4047. 영준이의 카드 카운팅/영준이의 카드 카운팅.py
+++ SWEA/D3/4047. 영준이의 카드 카운팅/영준이의 카드 카운팅.py
@@ -1,32 +1,17 @@
 t = int(input())
 for tc in range(1, t+1):
-    string = input()
-    n = len(string)
-    s, d, h, c = [0] * 14, [0] * 14, [0] * 14, [0] * 14
-    bools = True
-    for i in range(0, n, 3):
-        num = int(string[i+1:i+3])
-        if string[i] == 'S':
-            if s[num]:
-                bools = False
-                break
-            else: s[num] = 1
-        elif string[i] == 'D':
-            if d[num]:
-                bools = False
-                break
-            else: d[num] = 1
-        elif string[i] == 'H':
-            if h[num]:
-                bools = False
-                break
-            else: h[num] = 1
-        elif string[i] == 'C':
-            if c[num]:
-                bools = False
-                break
-            else: c[num] = 1
-    if bools:
-        print(f'#{tc} {13-sum(s)} {13-sum(d)} {13-sum(h)} {13-sum(c)}')
-    else :
-        print(f'#{tc} ERROR')
+    st = input()
+    arr = [[] for _ in range(4)]
+    dct = {'S' : 0, 'D' : 1, 'H' : 2, 'C' : 3}
+
+    for i in range(0, len(st), 3):
+        arr[dct[st[i]]].append(int(st[i+1:i+3]))
+
+    ans = []
+    for lst in arr:
+        if len(lst) != len(set(lst)):
+            print(f'#{tc} ERROR')
+            break
+        ans.append(13 - len(lst))
+    else:
+        print(f'#{tc}', *ans)
