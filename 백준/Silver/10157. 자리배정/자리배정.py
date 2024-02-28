6516dd1627899c244334facb8edcc351c28df734
JIAN LEE <jwm0307@daum.net>
2024-02-28 21:39:14 +0900

    [Silver IV] Title: 자리배정, Time: 132 ms, Memory: 116044 KB -BaekjoonHub

--- 백준/Silver/10157. 자리배정/자리배정.py
+++ 백준/Silver/10157. 자리배정/자리배정.py
@@ -1,27 +1,25 @@
+import sys
+input = sys.stdin.readline
+
 col, row = map(int, input().split()) # 갯수
 k = int(input())
 lst = [[0]*col for _ in range(row)]
-flag = True
 
 if col * row < k:
-    flag = False
-
-if flag:
+    print(0)
+else:
     # dir = 상 우 하 좌 반복
     dirx = [-1, 0, 1, 0]
     diry = [0, 1, 0, -1]
-    x, y, dir, dx, dy = row-1, 0, 0, -1, 0
+    x, y, dir = row-1, 0, 0
     for num in range(1, k+1): # num = 적을 숫자
-        lst[x][y] = num
         if num == k:
-            print(y+1,row-x)
+            print(y+1, row-x)
             break
-        nx, ny = x+dx, y+dy
+        lst[x][y] = num
+        nx, ny = x+dirx[dir], y+diry[dir]
         if 0<= nx < row and 0<= ny < col and not lst[nx][ny]: # 같은방향 가능하다면
             x, y = nx, ny
         else:
-            dir += 1
-            dx, dy = dirx[dir%4], diry[dir%4]
-            x, y = x+dx, y+dy
-else:
-    print(0)
+            dir = (dir+1)%4
+            x, y = x + dirx[dir], y + diry[dir]
