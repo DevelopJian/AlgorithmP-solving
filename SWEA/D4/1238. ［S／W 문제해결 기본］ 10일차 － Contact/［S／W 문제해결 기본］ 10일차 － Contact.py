b1b58d5facdfee20a8388f70caa2202d551c04e7
JIAN LEE <jwm0307@daum.net>
2024-03-01 00:35:46 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 185 ms, Memory: 59,580 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
@@ -1,22 +1,24 @@
 # visit 배열 중 최대숫자의 인덱스 중 젤큰것 구하기
-# 최대숫자가 여러명일 수도 있는 경우 고려 TODO() 뒤에서 부터 mx 인덱스 찾기? enumerate
+# 최대숫자가 여러명일 수도 있는 경우 고려
 from collections import deque
 def bfs(s):
     q = deque([s])
     v = [0] * 101
     v[s] = 1
+    mxidx = 0
+    mx = 0
     while q:
         n = q.popleft()
         for i in adj[n]:
             if not v[i]:
                 q.append(i)
                 v[i] = v[n] + 1
-    mx = max(v)
-    for idx, a in enumerate(v[::-1]):
-        if a == mx:
-            ans = 100 - idx
-            break
-    return ans
+                if v[i] == mx:
+                    if i > mxidx:
+                        mxidx = i
+                elif v[i] > mx:
+                    mx, mxidx = v[i], i
+    return mxidx
 
 for tc in range(1,11):
     l, start = map(int, input().split())
