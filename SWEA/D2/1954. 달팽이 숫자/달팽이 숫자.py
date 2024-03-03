4fe4d5157f164861eff1286ca73e6df52b14487d
JIAN LEE <jwm0307@daum.net>
2024-03-03 10:19:31 +0900

    [D2] Title: 달팽이 숫자, Time: 176 ms, Memory: 58,548 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,24 +1,20 @@
 t = int(input())
-for tc in range(1, t+1):
+for tc in range(1, 1+t):
     n = int(input())
-    lst = [[0]*n for _ in range(n)]
-
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
-
-    x, y, dir, num = 0, 0, 0, 1 # 초기화
-    lst[x][y] = num
-    num += 1
-
-    while num <= n**2:
-        nx, ny = x+dx[dir], y+dy[dir]
-        if 0<= nx <n and 0<= ny <n and lst[nx][ny] == 0:
+    arr = [[0]*n for _ in range(n)]
+    # 칸이 없거나 0이면 방향바꾸기
+    dx, dy = [0,1,0,-1],[1,0,-1,0]
+    dr = 0
+    x, y = 0, 0
+    for num in range(1, n**2+1):
+        arr[x][y] = num
+        nx, ny = x+dx[dr], y+dy[dr]
+        if 0<=nx<n and 0<=ny<n and arr[nx][ny] == 0:
             x, y = nx, ny
-            lst[x][y] = num
-            num += 1
         else:
-            dir = (dir+1) % 4
+            dr = (dr+1)%4
+            x, y = x + dx[dr], y + dy[dr]
 
     print(f'#{tc}')
-    for l in lst:
+    for l in arr:
         print(*l)
