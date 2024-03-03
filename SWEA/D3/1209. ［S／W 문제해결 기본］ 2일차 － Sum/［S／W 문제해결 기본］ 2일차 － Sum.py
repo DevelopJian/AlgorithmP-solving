1e4ae8ff955f9a1658e2f11317a8ad60eb91e2a9
JIAN LEE <jwm0307@daum.net>
2024-03-03 10:32:13 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 214 ms, Memory: 62,512 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.py
@@ -1,16 +1,16 @@
-for _ in range(10):
+for _ in range(1, 11):
     tc = input()
-    lst = [list(map(int, input().split())) for _ in range(100)]
-    lst2 = list(zip(*lst)) # 전치행렬
+    arr = [list(map(int, input().split())) for _ in range(100)]
+    arr2 = list(zip(*arr))
     mx = 0
-
-    # 행의 합, 열의 합 최댓값구하기
-    sm, sm2 = 0, 0
+    for row in range(100):
+        mx = max(mx, sum(arr[row]), sum(arr2[row]))
+    sm = 0
     for i in range(100):
-        mx = max(sum(lst[i]), sum(lst2[i]), mx)
-        sm += lst[i][i]
-        sm2 += lst[i][99-i]
-
-    mx = max(mx, sm, sm2)
-
+        sm += arr[i][i]
+    mx = max(sm, mx)
+    sm = 0
+    for i in range(100):
+        sm += arr[i][99-i]
+    mx = max(sm, mx)
     print(f'#{tc} {mx}')
