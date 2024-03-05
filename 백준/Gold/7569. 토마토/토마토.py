86cb5e523b04b464a62c6502d565694698f35e5f
JIAN LEE <jwm0307@daum.net>
2024-03-05 11:10:50 +0900

    [Gold V] Title: 토마토, Time: 528 ms, Memory: 153232 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -16,8 +16,10 @@ def bfs():
     elif len(q) == 0: # 초기에 익은게 하나도 없는 경우
         return -1
 
+    days = 0
     while q:
         x, y = q.popleft()
+        days = max(days, arr[x][y])
 
         # 사방에 있는 것 넣기
         for dx, dy in ((1,0),(-1,0),(0,1),(0,-1)):
@@ -32,13 +34,11 @@ def bfs():
                 arr[nx][ny] = arr[x][y] + 1
                 q.append((nx, ny))
 
-    # 익지않은게 있는지 확인 & 답구해주기
-    days = 0
+    # 익지않은게 있는지 확인
     for i in range(n * height):
         for j in range(m):
             if arr[i][j] == 0:
                 return -1
-            days = max(days, arr[i][j])
 
     return days - 1 # 1부터 시작했기 때문에 1 빼줌
 
