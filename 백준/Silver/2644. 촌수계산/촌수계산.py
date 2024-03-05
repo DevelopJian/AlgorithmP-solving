e72e5338a3fef40164641421f91f8467905ebb96
JIAN LEE <jwm0307@daum.net>
2024-03-05 09:25:19 +0900

    [Silver II] Title: 촌수계산, Time: 132 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,11 +1,11 @@
 from collections import deque
-
+# 몇칸 앞으로가는지
 n = int(input()) # 전체사람수
 a, b = map(int, input().split()) # 촌수계산해야하는 사람
 m = int(input())
-rlt = [[] for _ in range(n+1)] # 양방향 연결리스트
+rlt = [[] for _ in range(n+1)] # 연결리스트
 for _ in range(m):
-    x, y = map(int, input().split()) 
+    x, y = map(int, input().split())
     rlt[x].append(y)
     rlt[y].append(x)
 
@@ -15,10 +15,13 @@ def bfs(a, b): # 시작점, 도착점
     v[a] = 1
     while q:
         c = q.popleft()
+        if c == b:
+            break
         for nxt in rlt[c]:
             if v[nxt] == 0:
                 q.append(nxt)
                 v[nxt] = v[c] + 1
+
     return v[b]-1
 
 print(bfs(a,b))
