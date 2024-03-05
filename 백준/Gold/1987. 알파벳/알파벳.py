cb492b1901c4800c88de81d0f41ae0b76b2157c8
JIAN LEE <jwm0307@daum.net>
2024-03-05 16:31:58 +0900

    [Gold IV] Title: 알파벳, Time: 6488 ms, Memory: 173316 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -8,22 +8,18 @@ def dfs(cnt, x, y): # 방문한 칸수, 이전좌표
     # 사방탐색
     for dx, dy in ((0,1),(1,0),(0,-1),(-1,0)):
         nx, ny = x+dx, y+dy
-        if not v[nx][ny] and not alph[ord(arr[nx][ny]) - 65]: # 아스키코드 이용
-            v[nx][ny] = 1
+        # 1. 범위 내 2. 같은 알파벳 아닐 때 (지나온 칸은 어차피 알파벳 중복이니 칸에대한 visit 체크 필요없음)
+        if 0<= nx < r and 0<= ny < c and not alph[ord(arr[nx][ny]) - 65]: # 아스키코드 이용
             alph[ord(arr[nx][ny]) - 65] = 1
             dfs(cnt + 1, nx, ny)
-            v[nx][ny] = 0
             alph[ord(arr[nx][ny]) - 65] = 0
 
-r, c = map(int, input().split())
-arr = [[0]*(c+2)] + [[0]+list(input())+[0] for _ in range(r)] + [[0]*(c+2)]
-# visit 배열 : 가장자리 미리 visit 처리(범위체크 필요없게)
-v = [[1]*(c+2)] + [[1] + [0]*c + [1] for _ in range(r)] + [[1]*(c+2)]
-v[1][1] = 1
+r, c = map(int, input().strip().split())
+arr = [list(input().strip()) for _ in range(r)]
 alph = [0]*26
-alph[ord(arr[1][1]) - 65] = 1
+alph[ord(arr[0][0]) - 65] = 1
 mx = 0
 
-dfs(1, 1, 1)
+dfs(1, 0, 0)
 
 print(mx)
