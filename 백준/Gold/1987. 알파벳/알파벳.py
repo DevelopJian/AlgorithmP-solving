70d6010a914cb620e8aacaf6f05915ac7aa9af72
JIAN LEE <jwm0307@daum.net>
2024-03-05 17:27:04 +0900

    [Gold IV] Title: 알파벳, Time: 980 ms, Memory: 271932 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,25 +1,26 @@
-import sys
-input = sys.stdin.readline
+from collections import deque
 
-def dfs(cnt, x, y): # 방문한 칸수, 이전좌표
-    global mx
-    if cnt > mx:
-        mx = cnt
-    # 사방탐색
-    for dx, dy in ((0,1),(1,0),(0,-1),(-1,0)):
-        nx, ny = x+dx, y+dy
-        # 1. 범위 내 2. 같은 알파벳 아닐 때 (지나온 칸은 어차피 알파벳 중복이니 칸에대한 visit 체크 필요없음)
-        if 0<= nx < r and 0<= ny < c and not alph[ord(arr[nx][ny]) - 65]: # 아스키코드 이용
-            alph[ord(arr[nx][ny]) - 65] = 1
-            dfs(cnt + 1, nx, ny)
-            alph[ord(arr[nx][ny]) - 65] = 0
+def bfs():
+    q = deque([(0, 0, arr[0][0])]) # (이전좌표 행, 열, 지나온알파벳문자열)
+    v = [[set() for _ in range(c)] for _ in range(r)]
+    v[0][0].add(arr[0][0])
+    ans = 0
 
-r, c = map(int, input().strip().split())
-arr = [list(input().strip()) for _ in range(r)]
-alph = [0]*26
-alph[ord(arr[0][0]) - 65] = 1
-mx = 0
+    while q:
+        x, y, string = q.popleft()
+        # 정답처리
+        ans = max(ans, len(string))
 
-dfs(1, 0, 0)
+        for dx, dy in ((-1,0),(1,0),(0,1),(0,-1)):
+            nx, ny = x+dx, y+dy
+            # 1. 범위 2. 중복값 아닌경우, 중복 시퀀스 아닌 경우(같은 좌표에 있더라도 지나온 길에 따라 뒤의 결과 달라짐)
+            if 0<= nx <r and 0<= ny <c and arr[nx][ny] not in string\
+                    and (string + arr[nx][ny] not in v[nx][ny]):
+                v[nx][ny].add(string + arr[nx][ny])   # 왜 괄호 한번 더치지?
+                q.append((nx, ny, string + arr[nx][ny]))
+    return ans
 
-print(mx)
+r, c = map(int, input().split())
+arr = [list(input()) for _ in range(r)]
+
+print(bfs())
