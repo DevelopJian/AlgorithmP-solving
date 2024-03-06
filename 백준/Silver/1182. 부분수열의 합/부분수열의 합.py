9f0bf5502f33a7cbd8f8458022098881afa7f442
JIAN LEE <jwm0307@daum.net>
2024-03-06 20:41:57 +0900

    [Silver II] Title: 부분수열의 합, Time: 268 ms, Memory: 112272 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,16 +1,15 @@
-def dfs(idx, sm, cnt): # 들어있는 원소갯수
-    global result
-    # 종료조건
+def dfs(idx, sm, cnt):
+    global ans
     if idx == n:
         if sm == s and cnt > 0:
-            result += 1
+            ans += 1
         return
-    dfs(idx +1, sm +arr[idx], cnt+1) # 이번원소 넣는것
-    dfs(idx +1, sm, cnt) # 이번원소 안넣는것
+    dfs(idx + 1, sm + arr[idx], cnt + 1)
+    dfs(idx + 1, sm, cnt)
 
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
-result = 0
+ans = 0
 dfs(0, 0, 0)
 
-print(result)
+print(ans)
