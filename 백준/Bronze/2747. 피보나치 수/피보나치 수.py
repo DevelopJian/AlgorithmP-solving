8e55fd6fbfc6f8597ddb07f517318b103d5bdbf6
JIAN LEE <jwm0307@daum.net>
2024-03-10 11:54:45 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.py
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -2,9 +2,7 @@ n = int(input())
 fibo = [0] * (n+1)
 fibo[1] = 1
 
-x = 2
-while x <= n:
-    fibo[x] = fibo[x-2] + fibo[x-1]
-    x += 1
+for i in range(2, n+1):
+    fibo[i] = fibo[i-2] + fibo[i-1]
 
 print(fibo[n])
