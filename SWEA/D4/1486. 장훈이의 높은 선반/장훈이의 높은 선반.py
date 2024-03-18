d680a17796caaa552f268f524f695108c47beb3c
JIAN LEE <jwm0307@daum.net>
2024-03-18 13:52:15 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 287 ms, Memory: 63,312 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -1,25 +1,13 @@
-# 이진트리로 하기
-def dfs(idx, sm): # 직원 인덱스, 키의 합
-    global mn
-    # 가지치기(이미 정답가능성 없음)
-    if sm >= mn:
-        return
-    # 종료조건
-    if idx == n:
-        # 정답처리
-        if b <= sm < mn:
-            mn = sm
-        return
-    # dfs 호출
-    dfs(idx + 1, sm + h[idx])
-    dfs(idx + 1, sm)
+from itertools import combinations
 
 t = int(input())
 for tc in range(1, t+1):
     n, b = map(int, input().split())
     h = list(map(int, input().split()))
-    # 키의 합이 b이상인 것 중 제일 작은 것 구하기
-    mn = 10000 * n  # 키의 합 중 최소
-    dfs(0, 0)
+    mn = 10000 * n
+    for i in range(1, n+1): # i = 쌓은 점원수
+        for j in combinations(h, i):
+            if b<= sum(j) < mn:
+                mn = sum(j)
 
     print(f'#{tc} {mn-b}')
