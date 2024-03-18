ae2e8a1384ddb75036335114df2c0b99281ad0ae
JIAN LEE <jwm0307@daum.net>
2024-03-18 13:44:36 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 287 ms, Memory: 63,312 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -1,20 +1,25 @@
-def dfs(num, sm): # 몇번째점원 0~(n-1), 키 합
-    global mn_h
-    if sm >= mn_h: # 가지치기(백트래킹)
+# 이진트리로 하기
+def dfs(idx, sm): # 직원 인덱스, 키의 합
+    global mn
+    # 가지치기(이미 정답가능성 없음)
+    if sm >= mn:
         return
-    if num == n:
-        if sm >= b:
-            mn_h = min(sm, mn_h)
+    # 종료조건
+    if idx == n:
+        # 정답처리
+        if b <= sm < mn:
+            mn = sm
         return
-    dfs(num+1, sm + height[num]) # 이 점원 쌓였을 때
-    dfs(num+1, sm) # 이 점원 안쌓였을 떄
+    # dfs 호출
+    dfs(idx + 1, sm + h[idx])
+    dfs(idx + 1, sm)
 
 t = int(input())
 for tc in range(1, t+1):
     n, b = map(int, input().split())
-    height = list(map(int, input().split()))
-    mn_h = 200001
-
+    h = list(map(int, input().split()))
+    # 키의 합이 b이상인 것 중 제일 작은 것 구하기
+    mn = 10000 * n  # 키의 합 중 최소
     dfs(0, 0)
 
-    print(f'#{tc} {mn_h - b}')
+    print(f'#{tc} {mn-b}')
