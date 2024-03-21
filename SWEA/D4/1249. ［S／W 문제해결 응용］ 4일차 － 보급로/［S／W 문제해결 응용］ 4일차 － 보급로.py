b60eb0f4bf3af8092eaa4d3170ab0a0142eb1b5b
JIAN LEE <jwm0307@daum.net>
2024-03-21 13:55:42 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 218 ms, Memory: 71,244 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -1,26 +1,25 @@
 from collections import deque
- 
-def bfs(ei, ej):
-    q = deque([(0, 0)])
-    v = [['n'] * n for _ in range(n)]  # 'n' == 미방문
-    v[0][0] = 0
- 
-    while q:
-        i, j = q.popleft()
- 
-        for di, dj in ((1,0),(-1,0),(0,1),(0,-1)):
-            ni, nj = i + di, j + dj
-            if 0<= ni <n and 0<= nj <n: # 범위 내
-                if v[ni][nj] == 'n' or v[i][j] + arr[ni][nj] < v[ni][nj]:  # 미방문 or 갱신예정값이 더 작을 때 -> 갱신
-                    v[ni][nj] = v[i][j] + arr[ni][nj]
-                    q.append((ni, nj))
-                     
-    return v[ei][ej]
- 
+
 t = int(input())
 for tc in range(1, t+1):
     n = int(input())
     arr = [list(map(int, input())) for _ in range(n)]
- 
+
+    # BFS
+    q = deque([(0, 0)])
+    v = [['n'] * n for _ in range(n)]  # 'n' == 미방문
+    v[0][0] = 0
+
+    while q:
+        i, j = q.popleft()
+        # 정답처리? -> 끝까지 다 돌아야하니까 필요없음
+        for di, dj in ((1,0),(-1,0),(0,1),(0,-1)):
+            ni, nj = i + di, j + dj
+            if 0 <= ni < n and 0 <= nj < n: # 1. 범위 내
+                # 2. 미방문 or 갱신예정값이 더 작을 때 -> 갱신
+                if v[ni][nj] == 'n' or v[i][j] + arr[ni][nj] < v[ni][nj]:
+                    v[ni][nj] = v[i][j] + arr[ni][nj]
+                    q.append((ni, nj))
+
     # 출발지 (0,0), 도착지(n-1, n-1)
-    print(f'#{tc} {bfs(n-1, n-1)}')
+    print(f'#{tc} {v[n-1][n-1]}')
