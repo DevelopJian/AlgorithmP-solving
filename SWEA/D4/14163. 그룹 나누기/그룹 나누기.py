02c5fc2cb5f2ec2a7a23e68e5e6d17240cf8fd92
JIAN LEE <jwm0307@daum.net>
2024-03-21 11:58:19 +0900

    [D4] Title: 그룹 나누기, Time: 105 ms, Memory: 45,908 KB -BaekjoonHub

--- SWEA/D4/14163. 그룹 나누기/그룹 나누기.py
+++ SWEA/D4/14163. 그룹 나누기/그룹 나누기.py
@@ -1,17 +1,12 @@
-from collections import deque
-
-def bfs(s):
-    q = deque([s])
-
-    while q:
-        now = q.popleft()
-        for node in lst[now]:
-            if not v[node]:
-                v[node] = 1
-                q.append(node)
+def dfs(s):
+    for node in lst[s]:
+        if not v[node]:
+            v[node] = 1
+            dfs(node)
 
 t = int(input())
 for tc in range(1, t+1):
+
     n, m = map(int, input().split()) # 사람 수, 연결 수
     ipt = list(map(int, input().split()))
     lst = [[] for _  in range(n+1)] # 인접리스트 (양방향)
@@ -23,8 +18,8 @@ for tc in range(1, t+1):
     ans = 0
     for i in range(1, n+1):
         if not v[i]:
-            ans += 1  # bfs 함수 호출횟수
+            ans += 1  # dfs 함수 호출횟수
             v[i] = 1
-            bfs(i)
+            dfs(i)
 
     print(f'#{tc} {ans}')
