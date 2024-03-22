4766fb44921583a785fe6c8bd1017a620393fb94
JIAN LEE <jwm0307@daum.net>
2024-03-22 09:58:00 +0900

    [D4] Title: 정사각형 방, Time: 262 ms, Memory: 71,808 KB -BaekjoonHub

--- SWEA/D4/1861. 정사각형 방/정사각형 방.py
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.py
@@ -1,31 +1,39 @@
-# 1큰 방으로 이동
 from collections import deque
-def bfs(i, j):
-    global mx, mnval
-    q = deque([(i, j)])
-    ans = [arr[i][j]]
+def bfs(si, sj):
+    q = deque([(si, sj)])
+    mnroom = arr[si][sj]
+    move = 0
+
     while q:
         x, y = q.popleft()
+        mnroom = min(mnroom, arr[x][y])
+        move += 1
+
         for dx, dy in ((1,0),(-1,0),(0,1),(0,-1)):
-            nx, ny = x+dx, y+dy
-            if 0<=nx<n and 0<=ny<n and abs(arr[nx][ny]-arr[x][y])==1 and v[nx][ny] != 1:
-                q.append((nx, ny))
-                ans.append(arr[nx][ny])
-                v[nx][ny] = 1
-    # 큐에 아무것도 없으면
-    if len(ans) > mx or ( len(ans) == mx and min(ans)<mnval ):
-        mx, mnval = len(ans), min(ans)
+            nx, ny = x + dx, y + dy
+            if 0<= nx < n and 0 <= ny < n and not v[nx][ny]:
+                if abs(arr[nx][ny] - arr[x][y]) == 1:
+                    v[nx][ny] = 1
+                    q.append((nx, ny))
+    return move, mnroom
 
 t = int(input())
 for tc in range(1, t+1):
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]
     v = [[0]*n for _ in range(n)]
-    mx = 0
-    mnval = n*n+1
+
+    mxmove = start = 0
     for i in range(n):
         for j in range(n):
-            if not v[i][j]:
-                v[i][j] = 1
-                bfs(i,j)
-    print(f'#{tc} {mnval} {mx}')
+            if v[i][j]:
+                continue
+            v[i][j] = 1
+            move, mnroom = bfs(i, j)
+            if move == mxmove:
+                start = min(mnroom, start)
+            elif move > mxmove:
+                mxmove = move
+                start = mnroom
+
+    print(f'#{tc} {start} {mxmove}')
