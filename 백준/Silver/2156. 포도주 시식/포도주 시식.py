f67ca1d59d2021f7d8edc3330aa991fb1ca6a677
JIAN LEE <jwm0307@daum.net>
2024-03-26 00:10:50 +0900

    [Silver I] Title: 포도주 시식, Time: 752 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+if n == 1:
+    print(arr[0])
+elif n == 2:
+    print(arr[0] + arr[1])
+else:
+    dp = [0] * n
+    dp[0], dp[1] = arr[0], arr[0] + arr[1]
+    dp[2] = max(arr[0] + arr[2], arr[1] + arr[2], dp[1])
+    for i in range(3, n):
+        dp[i] = max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i], dp[i-1])
+
+    print(dp[-1])
