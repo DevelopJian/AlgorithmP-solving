b77c8ed5a64cd2ec2dfd37ab7adde7cd3b4481ff
JIAN LEE <jwm0307@daum.net>
2024-03-26 11:51:19 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 132 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 arr = list(map(int, input().split()))
 
@@ -6,14 +9,14 @@ dp = [[1]*n for _ in range(3)]
 # 올라가는 수열 dp[0]
 for i in range(1, n):
     for j in range(i):
-        if arr[i] > arr[j]:
-            dp[0][i] = max(dp[0][i], dp[0][j] + 1)
+        if arr[i] > arr[j] and dp[0][i] < dp[0][j] + 1:
+            dp[0][i] = dp[0][j] + 1
 
 # 내려가는 수열 dp[1]
 for i in range(n-1, -1, -1):
     for j in range(n-1, i, -1):
-        if arr[i] > arr[j]:
-            dp[1][i] = max(dp[1][i], dp[1][j] + 1)
+        if arr[i] > arr[j] and dp[1][i] < dp[1][j] + 1:
+            dp[1][i] = dp[1][j] + 1
 
 # 합 dp[2]
 for i in range(n):
