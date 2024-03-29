5fee1e63bc7b350b2255ef99264be4865886bb4b
JIAN LEE <jwm0307@daum.net>
2024-03-29 11:05:20 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 661 ms, Memory: 76,548 KB -BaekjoonHub

---
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 숫자 만들기 - 4008 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWIeRZV6kBUDFAVH) 
+
+### 성능 요약
+
+메모리: 76,548 KB, 시간: 661 ms, 코드길이: 1,079 Bytes
+
+### 제출 일자
+
+2024-03-29 11:05
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.py
@@ -0,0 +1,39 @@
+def dfs(num, total): # 이번에 연산할 숫자 인덱스, 지금까지 계산한 수
+    global mx, mn
+    # 종료조건, 정답처리
+    if num == n:
+        mx = max(total, mx)
+        mn = min(total, mn)
+        return
+    # 재귀호출
+    if opr[0]:
+        opr[0] -= 1
+        dfs(num + 1, total + nums[num])
+        opr[0] += 1 # 원상복구
+    if opr[1]:
+        opr[1] -= 1
+        dfs(num + 1, total - nums[num])
+        opr[1]  += 1
+    if opr[2]:
+        opr[2] -= 1
+        dfs(num + 1, total * nums[num])
+        opr[2] += 1
+    if opr[3] and nums[num]: # 나눌 숫자가 0이 아닐 때 나누기
+        opr[3] -= 1
+        # 음수 나누기 주의
+        if total * nums[num] < 0 and total % nums[num] != 0:
+            dfs(num + 1, total // nums[num] + 1)
+        else:
+            dfs(num + 1, total // nums[num])
+        opr[3] += 1
+
+t = int(input())
+for tc in range(1, t+1):
+    n = int(input()) # 숫자갯수
+    opr = list(map(int, input().split())) # n-1개 : + - * //
+    nums = list(map(int, input().split()))
+
+    mx, mn = float('-inf'), float('inf')
+    dfs(1, nums[0])
+
+    print(f'#{tc} {mx-mn}')
