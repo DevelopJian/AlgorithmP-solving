3dbab9752e9dbd6962c1d5ae4ad48f1db3d7121a
JIAN LEE <jwm0307@daum.net>
2024-03-29 16:58:04 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 314 ms, Memory: 70,956 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.py
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.py
@@ -9,20 +9,17 @@ for tc in range(1, t+1):
                 homes.append((i, j))
 
     mxhome = 1  # 손해 안보고 서비스 받을 수 있는 집 최대값 (k=1 이면 항상 한 집에 서비스 줄 수 있고 손해 안봄)
+
     for i in range(n):  # 중심 : (x, y)
         for j in range(n):
             x, y = i, j
 
-            dst = []  # 중심과의 거리 리스트
+            nums = [0] * 39
             for nx, ny in homes:
-                dst.append(abs(x-nx)+abs(y-ny))
+                nums[abs(nx-x) + abs(ny-y)] += 1
 
             for d in range(2*n-2, -1, -1):
-                avahome = 0  # 중심 (x,y) 일 때 제공가능 집 갯수
-                # 갯수세기
-                for ii in dst:
-                    if ii <= d:
-                        avahome += 1
+                avahome = sum(nums[0:d+1])
                 # 손해여부 확인
                 if avahome * m >= (d+1)*(d+1) + d*d:  # 지불비용 >= 드는비용  == 손해 안본다 -> break
                     break
