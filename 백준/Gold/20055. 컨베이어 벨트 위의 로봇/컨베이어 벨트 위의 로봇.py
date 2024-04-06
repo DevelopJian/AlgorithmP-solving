bc5f576a0ee5b1905f1f925f906b1147ea6c6cec
JIAN LEE <jwm0307@daum.net>
2024-04-06 18:04:21 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 428 ms, Memory: 116980 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -7,20 +7,24 @@ robot = deque([0] * n)
 cnt, ans = 0, 0 # 내구도, 단계
 while True:
     ans += 1
-    belt.rotate(1)
-    robot.rotate(1)
+    belt.rotate()
+    robot.rotate()
     robot[-1] = 0 # 마지막자리 로봇 없애주기
     for i in range(n-2, -1, -1):
         # 로봇 옯기기 : 1. 나한테 로봇있고 2. 우측 내구도 0보다 크고 2. 우측에 로봇 있다면
         if robot[i] == 1:
             if belt[i+1] > 0 and robot[i+1] == 0:
                 belt[i+1] -= 1
+                if belt[i+1] == 0:
+                    cnt += 1
                 robot[i+1] = 1
                 robot[i] = 0
     robot[-1] = 0 # 마지막자리 로봇 없애주기
     if belt[0] > 0: # 첫번째 자리 로봇 올려주기
         robot[0] = 1
         belt[0] -= 1
-    if belt.count(0) >= k:
+        if belt[0] == 0:
+            cnt += 1
+    if cnt >= k:
         break
 print(ans)
